9d89b41f17cb3816a6de0bb21e5c3515085b12bc
abhinand007 <33084097+abhinand007@users.noreply.github.com>
2019-03-15 15:23:09 +0530

    Update Q4_3.s

--- Q4_3.s
+++ Q4_3.s
@@ -2,5 +2,5 @@ main:
     add $s0, $zero, 115 
     add $s1, $zero, 98 
  
-    andi $s0, $s0, 112         #AND masking bits at 5th, 6th, 7th position 
-    ori $s1, $s1, 80        #Or masking bits at 5th and 7th position
+    andi $s0, $s0, 112        
+    ori $s1, $s1, 80      
