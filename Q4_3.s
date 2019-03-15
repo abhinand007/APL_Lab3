eb9a7f5019b8501af7817d730327a1f86755f24d
abhinand007 <33084097+abhinand007@users.noreply.github.com>
2019-03-15 16:20:22 +0530

    Update Q4_3.s

--- Q4_3.s
+++ Q4_3.s
@@ -1,6 +1,6 @@
 main: 
-    add $s0, $zero, 115 
-    add $s1, $zero, 98 
+    add $s0, $zero, 10
+    add $s1, $zero, 10 
  
-    andi $s0, $s0, 112        
-    ori $s1, $s1, 80      
+    andi $s0, $s0, 2        
+    ori $s1, $s1, 5      
